f4871e29ef45758d08eb8048f83ddac0d45ff711
Jorge Zapata <jorge.guridi@globant.com>
2016-03-11 16:15:19 -0600

    Added coffee script support

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var coffee = require('gulp-coffee');
+var gutil = require('gulp-util');
 
 gulp.task('styles', function() {
     gulp.src('css/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('coffee', function() {
+    gulp.src('app/coffee/**/*.coffee')
+        .pipe(coffee({bare:true}).on('error', gutil.log))
+        .pipe(gulp.dest('app/'));
 });
 
 gulp.task('default',function() {
